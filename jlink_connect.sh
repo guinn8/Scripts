1eaa49b371077dda73b15dcd2a0ef18699b343eb
Gavin Guinn <gavinguinn1@gmail.com>
2023-06-24 17:49:34 -0600

    ble and other scripts

--- jlink_connect.sh
+++ jlink_connect.sh
@@ -7,12 +7,13 @@
 # Brief: Connects to device over JLink
 # args:
 #   CMD_FILE: Execute JLink commandfile if supplied
+#   DEV_ID (optional): device id (default MIMXRT1021XXX5A)
 
 CMD_FILE=$1
-DEV_ID=MIMXRT1021XXX5A
+DEV_ID=${2:-MIMXRT1021XXX5A}  # default to MIMXRT1021XXX5A if not provided
 if [ -z "$CMD_FILE" ] 
     then
-        JLinkExe -Si swd -speed auto -AutoConnect 1 -Device $DEV_ID -NoGui 1
+        JLinkExe -Si swd -speed adaptive -AutoConnect 1 -Device $DEV_ID -NoGui 1
     else
         JLinkExe -Si swd -speed auto -AutoConnect 1 -Device $DEV_ID -CommandFile $CMD_FILE -NoGui 1
 fi
--- jlink_load.sh
+++ jlink_load.sh
@@ -8,9 +8,11 @@
 # args:
 #   BIN_FILE: binary file to load
 #   OFFSET: location in memory to load file (ex. 0x608000000)
+#   DEV_ID (optional): device id (default MIMXRT1021XXX5A)
 
 BIN_FILE=$1
 OFFSET=$2
+DEV_ID=${3:-MIMXRT1021XXX5A}  # default to MIMXRT1021XXX5A if not provided
 JLINK_SCRIPT=~/Scripts/temp.jlink
 
 # we need to use a temporary file to pass a command string to JLink
@@ -22,5 +24,5 @@ loadfile $BIN_FILE $OFFSET
 r
 exit" > $JLINK_SCRIPT
 
-source ~/Scripts/jlink_connect.sh $JLINK_SCRIPT
+source ~/Scripts/jlink_connect.sh $JLINK_SCRIPT $DEV_ID
 rm $JLINK_SCRIPT
